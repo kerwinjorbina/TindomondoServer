a61ad73692e123d391f14e7f9cde9674bb80f478
Arne Lapõnin <arne.laponin@gmail.com>
2016-02-03 13:34:18 +0200

    Routes, spec, crap

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   match '*all' => 'application#cors_preflight', via: :options
   scope '/api' do
     #routes go here
+    resources :events
+    resources :users
+    resources :registrations
   end
 end
