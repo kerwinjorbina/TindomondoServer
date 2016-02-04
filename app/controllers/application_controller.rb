15225e2a1097f0d49f751abadc85454adbb64a98
kerwinjorbina <kerwinjorbina@kerwinjorbina-Inspiron-3442.(none)>
2016-02-04 18:52:48 +0200

    adding cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+
+  skip_before_filter :verify_authenticity_token
+  before_filter :cors_preflight
+
+  def cors_preflight
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'GET,POST,PUT,PATCH,DELETE,OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Origin,Accept,Content-Type'
+    headers['Access-Control-Max-Age'] = '3628800'
+    head :ok, content_type: 'text/html' if request.request_method == 'OPTIONS'
+  end
 end
