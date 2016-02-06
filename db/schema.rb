ee3f0d295a1e03cb1d4c04f18c77c63cdb34abe6
kerwinjorbina <kerwinjorbina@kerwinjorbina-Inspiron-3442.(none)>
2016-02-06 17:01:15 +0200

    adding description to events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160204155658) do
+ActiveRecord::Schema.define(version: 20160206150047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20160204155658) do
     t.integer  "sport_id"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.string   "description"
   end
 
   add_index "events", ["sport_id"], name: "index_events_on_sport_id", using: :btree
