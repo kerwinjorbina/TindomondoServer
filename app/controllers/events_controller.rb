73301ea6a1837c51da2acd2bf3f849c12a5ba572
kerwinjorbina <kerwinjorbina@kerwinjorbina-Inspiron-3442.(none)>
2016-02-06 19:31:48 +0200

    adding description to the allowed parameters

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -73,6 +73,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:location, :start_time, :duration, :registration_min, :registration_limit, :user_id, :sport_id)
+      params.require(:event).permit(:location, :start_time, :duration, :registration_min, :registration_limit, :user_id, :sport_id, :description)
     end
 end
